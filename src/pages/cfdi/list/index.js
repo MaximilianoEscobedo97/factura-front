2560a8defd49595c0e7e2bccea814589302075e2
MaximilianoEscobedo97 <maximilianoescobedo97@gmail.com>
2021-06-23 17:16:22 -0500

    add dataTable run npm install

--- src/pages/cfdi/list/index.js
+++ src/pages/cfdi/list/index.js
@@ -1,5 +1,6 @@
 import react, { useState, useEffect }  from   'react'
 import {getAllCfdis} from "../../../services/main"
+import DataTable from 'react-data-table-component'
 
 export const ListCfdi = (proops) =>
 {
@@ -8,18 +9,67 @@ export const ListCfdi = (proops) =>
         cfdis();
     },[])
 
+    const [rows,setRows] = useState([]);
+
     const cfdis = async () =>
     {   
         let response = await getAllCfdis();
+        
+        if(!response)
+            return;
+        
         let data = response.data;
         let list = data.cfdis;
         console.log(list);
+        setRows(list);
+    }
+
+    const columns = [
+        {
+            name: 'ID',
+            selector: 'id',
+            sortable: true
+        },
+        {
+            name: 'Tipo Documento',
+            selector: 'TipoDocumento',
+            sortable: true
+        },
+        {
+            name: 'Uso CFDI',
+            selector: 'UsoCFDI',
+            sortable: true
+        },
+        {
+            name: 'Serie',
+            selector: 'Serie',
+            sortable: true
+        },
+        {
+            name: 'Status',
+            selector: 'status',
+            sortable: true
+        },
+        
+    ]
+    const paginationOptions ={
+        rowsPerPageText: 'Filas por pagina',
+        rangeSeparator: 'de',
+        selectAllRowsItem: true,
+        selectAllRowsText:'todos',
     }
     
 
     return(
         <>
         <h1>Esta es la vista de lista de CFDI</h1>
+        <DataTable
+         columns ={columns}
+         data = {rows}
+         title ={'Lista de CFDIS'}
+         pagination
+         paginationComponentOptions = {paginationOptions}
+        />
         </>
     )
 }
